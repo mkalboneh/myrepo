85e721879336d2a5ac051d864f63b922c795a54c
Mohammad Kalboneh <m.kalboneh@yamsafer.me>
2014-06-27 17:46:11 +0300

    first commit

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookRedirectLoginHelper;
+
+class HomeController extends BaseController {
+
+	public function showWelcome()
+	{
+		return View::make('hello');
+	}
+
+	public function authenticate()
+	{
+		echo "Hello";
+		$session = FacebookSession::setDefaultApplication('342442919241949', 'a2fe98cd32944981c660d759e7e8eb66');
+		// dd($session);
+		$helper = new FacebookRedirectLoginHelper('www.google.com');
+		$loginUrl = $helper->getLoginUrl();
+	}
+
+}
